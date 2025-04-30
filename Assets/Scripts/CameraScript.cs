d4913e54513a0a7e79ab981dc26183314ad78e62
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-04-30 14:44:17 +0300

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraAnchor;   // точка прив'язки камери 
+    // за якою вона рухається, навколо якої обертається та до якої наближається
+    private Vector3 offset;  // вектор взамного розміщення персонажа та камери
+
+    private InputAction lookAction;   // Рухи маніпулятора "миша"
+    private float rotAngleY;
+    private float rotSensitivityY = 10f;
+    private float rotAngleX;
+    private float rotSensitivityX = 5f;
+
+    void Start()
+    {
+        offset = cameraAnchor.position - transform.position ;
+        lookAction = InputSystem.actions.FindAction("Look");
+        rotAngleY = transform.eulerAngles.y;
+        rotAngleX = transform.eulerAngles.x;
+    }
+
+    void Update()
+    {
+        Vector2 lookValue = Time.deltaTime * lookAction.ReadValue<Vector2>();
+        //     new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        rotAngleY += rotSensitivityY * lookValue.x;
+        rotAngleX -= rotSensitivityX * lookValue.y;
+
+        transform.eulerAngles = new Vector3(rotAngleX, rotAngleY, 0f);  // самого лише
+        // обертання камери недостатньо, оскільки вона губить персонажа з 
+        // поля зору через наявність зміщення offset -- його теж треба 
+        // повертати разом з поворотом камери
+
+        transform.position = cameraAnchor.position - // offset : без корекції на поворот
+            Quaternion.Euler(0f, rotAngleY, 0f) * offset;
+    }
+}
+/* Управління камерою.
+ * Основа - положення персонажа, а також
+ * рухи миші, які обертають камеру.
+ */
+/* Д.З. Підібрати граничні кути для повороту камери 
+ * по вертикалі
+ * - не бачить горизонт
+ * - не випускає персонаж з поля зору
+ * Впровадити обмеження на обертання згідно з визначеними
+ * граничними кутами.
+ */
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private InputAction moveAction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        // rb.AddForce(moveValue.x, 0, moveValue.y);  -- прив'язка до світових осей 
+        // (напрямів) -- незалежно від повороту камери рух іде вздовж постійних напрямів
+
+        // орієнтація за камерою:
+        Vector3 cameraForward = Camera.main.transform.forward;
+        cameraForward.y = 0f;   // прибираємо вертикальну компоненту (проєктуємо на 
+        // горизонтальну площину)
+        if (cameraForward == Vector3.zero)  // на випадок якщо камера ідеально згори
+        {                                   // тоді замінюємо forward на up
+            cameraForward = Camera.main.transform.up;
+        }
+        else
+        {
+            cameraForward.Normalize();      // призводимо вектор до одиничного розміру (видовжуємо)
+        }
+
+        Vector3 cameraRight = Camera.main.transform.right;   // корегування не потребує
+        // оскільки завжди має бути горизонтальним
+
+        rb.AddForce(moveValue.x * cameraRight + moveValue.y * cameraForward);
+    }
+}
+/* Скрипт управління персонажем.
+ * Базується на фізиці прикладання сили до 
+ * сферичного тіла, що може котитись
+ */
